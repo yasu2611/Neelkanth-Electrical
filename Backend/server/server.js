bb6bd278fdd22def7b8004b483660b76b0eb5c6f
yasu2611 <yashchavda2711@gmail.com>
2026-08-10 19:30:08 +0530

    Updated server.js

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -73,8 +73,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`✅ Server running on http://localhost:${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`✅ Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
